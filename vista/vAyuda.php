74e06fcf19a2340ee62807922c1fe3406baaad90
ismael <ismael.maecar@educa.jcyl.es>
2022-06-12 15:52:21 +0000

    vAyuda okk

--- vista/vAyuda.php
+++ vista/vAyuda.php
@@ -3,9 +3,9 @@
 
 <h3>Ayuda</h3>
 <h2>¿En qué podemos ayudarte?</h2>
-
+<br>
 <!-- Apartados de Ayuda -->
-<div class="accordion accordion-flush acordeon" id="accordionFlushExample">
+<div class="accordion accordion-flush acordeon mb-3" id="accordionFlushExample">
   <!-- Publicar un Anuncio -->
   <div class="accordion-item">
     <h2 class="accordion-header" id="flush-headingOne">
@@ -15,7 +15,12 @@
     </h2>
     <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
       <div class="accordion-body">
-        <p>Publicar un Anuncio</p>
+        <h5>Publicar un Anuncio</h5>
+        <p>1º - Acceder a la pantalla inicial de la Aplicación (Inicio).</p>
+        <p>2º - Pulsar sobre el botón flotante situado en la parte inferior derecha.</p>
+        <p>3º - Rellenar el formulario con todos los datos (incluyendo las 2 imágenes).</p>
+        <p>4º - Pulsar sobre el botón 'Crear' situado en la parte inferior del formulario.</p>
+        <p>5º - Los anuncios se visualizarán desde la pantalla inicial (Inicio) ordenador por fecha de publicación.</p>
       </div>
     </div>
   </div>
@@ -28,7 +33,12 @@
     </h2>
     <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
       <div class="accordion-body">
-        <p>Eliminar un Anuncio</p>
+        <h5>Eliminar un Anuncio</h5>
+        <p>1º - Acceder a la pantalla inicial de la Aplicación (Inicio).</p>
+        <p>2º - Pulsar sobre el botón 'Ver Producto' del Anuncio que se quiere eliminar para acceder al detalle.</p>
+        <p>3º - Pulsar sobre el botón 'Eliminar' situado en la parte inferior.</p>
+        <p>4º - Confirmar la acción sobre el diálogo emergente.</p>
+        <p><i><b>Nota:</b>&nbsp;El usuario solo podrá eliminar aquellos Anuncios de los cuales sea poseedor.</i></p>
       </div>
     </div>
   </div>
@@ -41,8 +51,13 @@
     </h2>
     <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
       <div class="accordion-body">
-        <p>Comentar un Anuncio</p>
-      </div>
+        <h5>Comentar un Anuncio</h5>
+        <p>1º - Acceder a la pantalla inicial de la Aplicación (Inicio).</p>
+        <p>2º - Pulsar sobre el botón 'Ver Producto' del Anuncio que se quiere comentar para acceder al detalle.</p>
+        <p>3º - Pulsar sobre el botón 'Comentar' situado en la parte inferior.</p>
+        <p>4º - Escribir el texto a comentar en el textarea el diálogo emergente.</p>
+        <p>5º - Confirmar la acción pulsando sobre el botón 'Comentar' del diálogo emergente.</p>
+        </div>
     </div>
   </div>
   <!-- Contactar con un Anunciante -->
@@ -54,23 +69,51 @@
     </h2>
     <div id="flush-collapseFour" class="accordion-collapse collapse" aria-labelledby="flush-headingFour" data-bs-parent="#accordionFlushExample">
       <div class="accordion-body">
-        <p>Contactar con un Anunciante</p>
+        <h5>Contactar con un Anunciante</h5>
+        <p>1º - Acceder a la pantalla inicial de la Aplicación (Inicio).</p>
+        <p>2º - Acceder al detalle de una publicación de dicho usuario Anunciante.</p>
+        <p>3º - Pulsar sobre el botón 'Ver Perfil' situado en la parte inferior.</p>
+        <p>4º - Una vez en el perfil, acceder a sus datos (e-mail).</p>
+        <p>5º - Redactar un email a dicho usuario a través del servicio de mensajería electrónica deseado.</p>
+        </div>
       </div>
     </div>
-  </div>
   <!-- Filtrar Anuncios -->
   <div class="accordion-item">
     <h2 class="accordion-header" id="flush-headingFive">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFive" aria-expanded="false" aria-controls="flush-collapseFive">
-        Filtrar Anuncios
+        Filtrar un Anuncio
       </button>
     </h2>
     <div id="flush-collapseFive" class="accordion-collapse collapse" aria-labelledby="flush-headingFive" data-bs-parent="#accordionFlushExample">
       <div class="accordion-body">
-        <p>Filtrar Anuncios</p>
+        <h5>Filtrar un Anuncio</h5>
+        <p>1º - Acceder a la pantalla inicial de la Aplicación (Inicio).</p>
+        <p>2º - Pulsar sobre el botón 'Filtrar' situado en la barra de navegación superior.</p>
+        <p>3º - Establecer los criterios de filtrado deseados en el formulario del diálogo emergente.</p>
+        <p>4º - Pulsar sobre el botón 'Filtrar' situado en la parte inferior.</p>
+        <p>5º - Los anuncios filtrados según el criterio de búsqueda serán desplegados en una nueva pantalla.</p>
+        </div>
       </div>
     </div>
-  </div>
+    <!-- Buscar Anuncios -->
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="flush-headingBusq">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseBusq" aria-expanded="false" aria-controls="flush-collapseBusq">
+        Buscar un Anuncio
+      </button>
+    </h2>
+    <div id="flush-collapseBusq" class="accordion-collapse collapse" aria-labelledby="flush-headingBusq" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body">
+        <h5>Filtrar un Anuncio</h5>
+        <p>1º - Acceder a la pantalla inicial de la Aplicación (Inicio).</p>
+        <p>2º - Escribir sobre el recuadro de texto 'Busca tu Anuncio' situado en la barra de navegación superior.</p>
+        <p>3º - Introducir el criterio de búsqueda deseado (la búsqueda se realiza sobre el título del Anuncio).</p>
+        <p>4º - Pulsar sobre el botón 'Buscar' situado en la parte derecha de la barra de Navegación.</p>
+        <p>5º - Los anuncios buscados según el criterio de búsqueda serán desplegados en una nueva pantalla.</p>
+        </div>
+      </div>
+    </div>
   <!-- Editar el Perfil -->
   <div class="accordion-item">
     <h2 class="accordion-header" id="flush-headingSix">
@@ -80,7 +123,12 @@
     </h2>
     <div id="flush-collapseSix" class="accordion-collapse collapse" aria-labelledby="flush-headingSix" data-bs-parent="#accordionFlushExample">
       <div class="accordion-body">
-        <p>Editar el Perfil</p>
+        <h5>Editar el Perfil</h5>
+        <p>1º - Acceder a la pantalla inicial de la Aplicación (Inicio).</p>
+        <p>2º - Pulsar sobre el botón 'Perfil' situado en la barra de navegación superior.</p>
+        <p>3º - Una vez dentro del Perfil, pulsar sobre el botón 'Modificar' situado en la parte inferior.</p>
+        <p>4º - Modificar los datos necesarios sobre el formulario dado, incluyendo la imagen del perfil.</p>
+        <p>5º - Pulsar sobre el botón 'Confirmar' situado en la parte inferior.</p>
       </div>
     </div>
   </div>
@@ -93,7 +141,11 @@
     </h2>
     <div id="flush-collapseSeven" class="accordion-collapse collapse" aria-labelledby="flush-headingSeven" data-bs-parent="#accordionFlushExample">
       <div class="accordion-body">
-        <p>Cerrar Sesión</p>
+      <h5>Cerrar Sesión</h5>
+        <p>1º - Acceder a la pantalla inicial de la Aplicación (Inicio).</p>
+        <p>2º - Pulsar sobre el botón 'Cerrar Sesión' situado en la barra de navegación superior.</p>
+        <p>3º - Confirmar la acción pulsando sobre el botón 'Cerrar Sesión' situado en la parte inferior.</p>
+        <p>4º - Tras ello, será redirigido a la pantalla de inicio, desde la cual podrá iniciar sesión de nuevo.</p>
       </div>
     </div>
   </div>
